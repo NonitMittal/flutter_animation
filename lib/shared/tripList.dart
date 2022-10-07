2de24c70793d3d4697a3b933e2c5a7a8eea33bfe
Nonit Mittal <nonitmittal@gmail.com>
2022-10-07 21:42:50 +0530

    Tween Animation in List Items

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -33,40 +33,57 @@ class _TripListState extends State<TripList> {
   }
 
   Widget _buildTile(Trip trip) {
-    return ListTile(
-      onTap: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => Details(trip: trip)));
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: const Duration(milliseconds: 1000),
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+              padding: EdgeInsets.only(
+                  top: value * 25, left: value * 15, right: value * 15),
+              child: child),
+        );
       },
-      contentPadding: const EdgeInsets.all(30),
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            '${trip.nights} nights',
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue[300],
+      child: ListTile(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => Details(trip: trip)));
+        },
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '${trip.nights} nights',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue[300],
+              ),
+            ),
+            Text(
+              trip.title,
+              style: TextStyle(fontSize: 20, color: Colors.grey[600]),
+            ),
+          ],
+        ),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Hero(
+            tag: 'location-img-${trip.img}',
+            child: Image.asset(
+              'images/${trip.img}',
+              height: 70.0,
             ),
           ),
-          Text(
-            trip.title,
-            style: TextStyle(fontSize: 20, color: Colors.grey[600]),
-          ),
-        ],
-      ),
-      leading: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Hero(
-          tag: 'location-img-${trip.img}',
-          child: Image.asset(
-            'images/${trip.img}',
-            height: 50.0,
-          ),
+        ),
+        trailing: Text(
+          '\$ ${trip.price}',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      trailing: Text('\$${trip.price}'),
     );
   }
 

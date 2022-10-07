c7665501ac20d266ff962ddda9596a82c58bd81f
Nonit Mittal <nonitmittal@gmail.com>
2022-10-07 21:04:21 +0530

    tween animation on title

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,12 +7,23 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 36,
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: const Duration(milliseconds: 1000),
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child:
+              Padding(padding: EdgeInsets.only(top: value * 25), child: child),
+        );
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+            fontSize: 36,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5),
       ),
     );
   }
